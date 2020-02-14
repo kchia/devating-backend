ef8f220736bf0f67102f9af12c1f46a7f5c7b5eb
jadekang <jkang.se@gmail.com>
2020-02-14 12:28:29 -0800

    initial setup with index.js and other directories

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+const userController = require('./controllers/userController');
+
+const cors = require('cors');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.set('port', process.env.PORT || 7000);
+
+app.listen(app.get('port'), () => {
+  console.log(`✅ PORT: ${app.get('port')} 🌟`);
+});
