61fe6f62a7d01c0c1aa364fa8951537f524f352b
jadekang <jkang.se@gmail.com>
2020-02-14 23:34:12 -0800

    matching algorithm complete

--- controllers/userController.js
+++ controllers/userController.js
@@ -13,6 +13,8 @@ router.get('/:email', (req, res) => {
   // declare current user
   let currentUser;
   let matchedUsers = [];
+  let finalUsers = [];
+  let isMatched = false;
   User.find({ email: req.params.email }).then(user => {
     currentUser = user;
     User.find({ gender: user[0].genderInterest }).then(users => {
@@ -21,11 +23,46 @@ router.get('/:email', (req, res) => {
           users[i].genderInterest === currentUser[0].gender &&
           users[i].email !== currentUser[0].email
         ) {
-          matchedUsers.push(users[i]);
+          for (let j = 0; j < users[i].favoriteCoding.length; j++) {
+            for (let k = 0; k < users[i].favoriteCoding.length; k++) {
+              if (
+                currentUser[0].favoriteCoding[j].id ===
+                users[i].favoriteCoding[k].id
+              ) {
+                isMatched = true;
+              }
+            }
+          }
+          if (isMatched == true) {
+            matchedUsers.push(users[i]);
+            isMatched = false;
+          }
         }
       }
-      res.json(matchedUsers);
+      isMatched = false;
+
+      for (let l = 0; l < matchedUsers.length; l++) {
+        for (let m = 0; m < matchedUsers[l].favoriteActivities.length; m++) {
+          for (let n = 0; n < matchedUsers[l].favoriteActivities.length; n++) {
+            if (
+              currentUser[0].favoriteActivities[m].id ===
+              users[l].favoriteActivities[n].id
+            ) {
+              isMatched = true;
+            }
+          }
+        }
+        if (isMatched == true) {
+          finalUsers.push(matchedUsers[l]);
+          isMatched = false;
+        }
+      }
+
+      res.json(finalUsers);
     });
+    //match the currentUser to other users who have one of matching favorite codings
+
+    //match the currentUser to other users who have one of matching favorite activities
   });
   // collect the gender interests of all other users
 
