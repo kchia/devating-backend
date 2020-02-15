8eb146a0c9a5617a842821e310b7264d603cd1f0
Sage-Kearney <56275819+Sage-Kearney@users.noreply.github.com>
2020-02-15 17:06:03 -0500

    added update & delete routes to userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,10 +3,23 @@ const router = express.Router();
 
 const User = require('../db/models/userModel');
 
+// create new user
 router.post('/signup', (req, res) => {
     User.create(req.body).then(users => {
         res.json(users);
     });
 });
+
+// update user information
+router.put('/:id/edit' , (req, res) => {
+  User.findByIdAndUpdate({ _id: req.params.id }, req.body, { new: true })
+  .then(user => res.json(user));
+})
+
+// delete user
+router.delete('/:id/delete', (req, res) => {
+  User.findByIdAndDelete({ _id: req.params.id})
+  .then(user => res.json(user));
+})
 
 module.exports = router;
