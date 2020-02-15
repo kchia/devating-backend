1992eb5a8e9ff1318815ea9181b5d1bcbb73c72a
Sage-Kearney <56275819+Sage-Kearney@users.noreply.github.com>
2020-02-14 20:00:00 -0500

    Working on match logic

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,11 +7,34 @@ router.get('/', (req, res) => {
   User.find({}).then(users => {
     res.json(users);
   });
-});
+}); 
+
 router.get('/:email', (req, res) => {
-  User.find({ email: req.params.email }).then(users => {
-    res.json(users);
+  // declare current user
+  let currentUser = User.find({ email: req.params.email }).then(users => {
+    res.json(users)
+    console.log(users);
+    let currentUserInterest = user.genderInterest;
+     
+    // collect the gender interests of all other users
+    router.get('/', (req, res) => {
+      let otherUsersGI = User.find({ genderInterest: req.params.genderInterest }).then(users => {
+      res.json(users);
+      
+      for(let i = 0; i < otherUsersGI.length; i++){
+        if (currentUserInterest !== otherUsersGI){
+          let matchedUsers = [];
+          matchedUsers.push(i);
+        } return matchedUsers;
+      }
+    });
   });
+  })
+
+  // find gender interest of currentUser and return all users whose gender interest !== currentUser.genderInterest
+
+  // let matchedUsers = 
+
 });
 
 module.exports = router;
