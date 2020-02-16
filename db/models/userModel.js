a70d90e8e2eb26a7585c929791e3ac65065f0a33
jadekang <jkang.se@gmail.com>
2020-02-15 21:39:01 -0800

    modified the userSchema with some requirements and added validator package

--- db/models/userModel.js
+++ db/models/userModel.js
@@ -1,9 +1,16 @@
 const mongoose = require('../connection');
+const validator = require('validator');
 
 const UserSchema = new mongoose.Schema({
-  image: { type: String, required: true },
-  email: { type: String, required: true },
-  name: { type: String, required: true },
+  image: String,
+  email: {
+    type: String,
+    required: [true, 'Please provide your email'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email']
+  },
+  name: { type: String, required: [true, 'Please tell us your name'] },
   age: { type: Number, required: true },
   gender: { type: String, required: true },
   favoriteActivities: [
@@ -20,8 +27,15 @@ const UserSchema = new mongoose.Schema({
   ],
   genderInterest: { type: String, required: true },
   keep: [],
-  password: { type: String, required: true },
-  passwordConfirm: { type: String, required: true }
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minlength: 8
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password']
+  }
 });
 
 const User = mongoose.model('User', UserSchema);
