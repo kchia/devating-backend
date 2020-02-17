10452594cb41bba5df5dd71d4888d8c6afe8833d
daisyy125 <daisy125@gmail.com>
2020-02-17 16:09:56 -0500

    match.test testing

--- Test/match.test.js
+++ Test/match.test.js
@@ -3,3 +3,16 @@ const expect = require('chai').expect;
 const supertest = require('supertest');
 
 const api = supertest('http://localhost:7000');
+
+describe('GET /', () => {
+    it('should return a 200 response', done => {
+        api.get('/').set('Accept', 'application/json').expect(200, done);
+    });
+
+    it('should return an array of all users', done => {
+        api.get('/').set('Accept', 'application/json').end((error, response) => {
+            expect(response.body).to.be.an('array');
+            done();
+        });
+    });
+})
