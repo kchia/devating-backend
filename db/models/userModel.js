d785772d461f58f05ea0b2c47d7190bc5b1d954e
jadekang <jkang.se@gmail.com>
2020-02-17 16:55:50 -0800

    fixed errors and model updated

--- db/models/userModel.js
+++ db/models/userModel.js
@@ -1,40 +1,33 @@
 const mongoose = require('../connection');
 const validator = require('validator');
 
-const UserSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema({  
   image: String,
   email: {
     type: String,
-    required: [true, 'Please provide your email'],
     unique: true,
     lowercase: true,
     validate: [validator.isEmail, 'Please provide a valid email']
   },
-  name: { type: String, required: [true, 'Please tell us your name'] },
-  age: { type: Number, required: true },
-  gender: { type: String, required: true },
+  name: { type: String },
+  age: { type: Number },
+  gender: { type: String },
   favoriteActivities: [
     {
-      image: { type: String, required: true },
-      id: { type: Number, required: true }
+      image: { type: String },
+      id: { type: Number }
     }
   ],
   favoriteCoding: [
     {
-      image: { type: String, required: true },
-      id: { type: Number, required: true }
+      image: { type: String },
+      id: { type: Number }
     }
   ],
-  genderInterest: { type: String, required: true },
+  genderInterest: { type: String },
   keep: [],
   password: {
-    type: String,
-    required: [true, 'Please provide a password'],
-    minlength: 8
-  },
-  passwordConfirm: {
-    type: String,
-    required: [true, 'Please confirm your password']
+    type: String
   }
 });
 
